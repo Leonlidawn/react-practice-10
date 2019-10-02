9c6a5c00afaf0ca07eb4317bcab0be8682b4c1e7
Weidong Li <leonlidawn@gmail.com>
2019-10-02 17:30:55 +1000

    how to get onClick values?

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greeting from './Greeting';
 import Stopwatch from './Stopwatch';
+import SignUpForm from './SignUpForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Stopwatch />
+        <SignUpForm />
 
       </header>
     </div>
---
+++ src/signUpForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './SignUpForm.js'
+
+class SignUpForm extends React.Component {
+  constructor() {
+    super();
+    this.state = { 
+      username: '',
+      password:''
+     };
+  }
+
+  changeInput = (e) => {
+    console.log(e.target.value)
+
+    this.setState({
+      //这个可以用变量代表key，用法是[keynameVar]
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+
+      console.log("username:"+e.target.username.value); 
+      console.log("passowrd:"+e.target.passowrd.value); 
+    
+    // console.log('Your name is', e.target.username.value); 
+
+    this.setState(
+      {
+        username:'',
+        passowrd:''
+      }
+    )
+    
+  }
+
+  render() {
+    const {username, password} = this.state;
+    return (
+
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">username: </label>
+        <input placeholder="please enter username" type="text" name="username" id="username" value={username} onChange={this.changeInput} />
+
+        <label htmlFor="password">password: </label>
+        <input autoComplete="off" placeholder="please enter password" type="password" name="password" id="password" value={password} onChange={this.changeInput} />
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
+}
+export default SignUpForm;
